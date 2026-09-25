9f2ff50fac0fff86304c462853c617ca2dada13a
radhekant230-debug <radhekant230@gmail.com>
2026-09-25 21:33:15 +0530

    Update db.js

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,16 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const connect = () => {
-	// Prefer a full connection string if provided (simplest option).
-	if (process.env.MONGO_URI) {
-		return mongoose.connect(process.env.MONGO_URI);
-	}
-
-	// Otherwise build it from separate pieces, with the cluster address
-	// also coming from .env instead of being hardcoded.
-	return mongoose.connect(
-		`mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@${process.env.ATLAS_CLUSTER}/tanishq`
-	);
+    return mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connect;
